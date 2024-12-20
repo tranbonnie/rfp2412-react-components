851c943b92dd765cbf7ee9105ed1fd7cbcc22413
tranbonnie <tran.bonniee@gmail.com>
2024-12-20 12:56:14 -0800

    got react components working

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // TODO
+
+//mine
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const style = {
+    fontWeight: isHovered ? 'bold' : 'normal',
+  };
+
+  return (
+    <li style = {style} onMouseEnter = {() => setIsHovered(isHovered)} onMouseLeave = {() => setIsHovered(!isHovered)}>{props.groceryListItem}</li>
+  );
+};
+
+const GroceryList = () => {
+  const groceryItems = ['Chicken Nuggets', 'Coke Zero', 'Protein Powder', 'Asparagus'];
+
+  return (
+    <ul>
+      {groceryItems.map((item) => (
+        <GroceryListItem groceryListItem = {item} />
+      ))}
+    </ul>
+  )
+};
+
+
+ReactDOM.render(
+  <GroceryList />,
+  document.getElementById('app')
+)
+
+// const Cucumbers = () => (<li>Cucumbers</li>);
+// const Kale = () => (<li>Kale</li>);
+
+// const GroceryList = () =>
+//   (  <ul>
+//       <GroceryListItem groceryListItem ={'Chicken Nuggets'}/>
+//      </ul>);
